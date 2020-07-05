151a3ff7f0ec918fe821c9b9c3704c7a7e3d778b
Prof. Dr. Christian Pfitzner <christian.pfitzner@thi.de>
2020-07-05 23:40:21 +0200

    -- adding two example to learn rust

---
+++ check_repos.rs
@@ -0,0 +1,38 @@
+
+
+// this tool show you if all repositories are upd to date
+use std::process;
+use std::process::Command; 
+use std::io;
+use std::fs::{self, DirEntry};
+use std::path::Path;
+
+use std::ops::Not;
+
+
+fn main()
+{
+
+let git_folder = Path::new("/home/chris/workspace/gi"); 
+
+
+if git_folder.exists().not() 
+{
+    println!("path does not exist. Abortin program. ");
+    process::exit();
+}
+
+if git_folder.is_dir()
+{
+    println!("path is directory");
+}
+
+
+// output of the path to the terminal
+println!("{}", git_folder.display());
+
+
+
+
+
+}
